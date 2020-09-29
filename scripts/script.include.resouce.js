0f734d4391878c6bdb5ee61d8efc690bde068e7f
AgencyDesign7 <62264621+AgencyDesign7@users.noreply.github.com>
2020-09-28 21:50:30 -0300

    initial commit

---
+++ scripts/script.include.resouce.js
@@ -0,0 +1,8 @@
+!function createHead() {
+    var headerElement = document.createElement('link');
+    headerElement.rel = "stylesheet";
+    headerElement.type = "text/css";
+    headerElement.href = './css/style.root.css';
+    document.getElementsByTagName('head')[0].appendChild(headerElement);
+}()
+
